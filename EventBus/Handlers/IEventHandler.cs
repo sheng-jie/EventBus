743705ce7e4d09c71d9213117a80fc9301f1de9e
圣杰 <ysjshengjie@live.cn>
2017-06-07 11:29:36 +0800

    初始化项目结构

---
+++ EventBus/Handlers/IEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Handlers
+{
+    /// <summary>
+    /// 定义事件处理器公共接口，所有的事件处理都要实现该接口
+    /// </summary>
+    public interface IEventHandler
+    {
+    }
+
+    /// <summary>
+    /// 泛型事件处理器接口
+    /// </summary>
+    /// <typeparam name="TEventData"></typeparam>
+    public interface IEventHandler<TEventData> : IEventHandler where TEventData : IEventData
+    {
+        /// <summary>
+        /// 事件处理器实现该方法来处理事件
+        /// </summary>
+        /// <param name="eventData"></param>
+        void HandleEvent(TEventData eventData);
+    }
+}
