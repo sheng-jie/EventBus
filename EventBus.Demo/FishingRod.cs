ac42a39121af0803b6c266d33003f94440923817
shengjie_yan <ysjshengjie@live.cn>
2017-06-08 07:04:27 +0800

    反射注册事件，未完待续

--- EventBus.Demo/FishingRod.cs
+++ EventBus.Demo/FishingRod.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace EventBus.Demo
 {
@@ -9,6 +10,21 @@ namespace EventBus.Demo
     {
         public delegate void FishingHandler(FishingEventData eventData); //声明委托
         public event FishingHandler FishingEvent; //声明事件
+
+        public FishingRod()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            foreach (var type in assembly.GetTypes())
+            {
+                Type eventHandlerType = type.GetInterface("IEventHandler`1");//获取实现了IEventHandler的类型
+                if (eventHandlerType != null) //若是事件处理者
+                {
+                    Type eventType = eventHandlerType.GetGenericArguments()[0]; // 获取IEventHandler的参数类型
+                    //未完待续
+                }
+            }
+        }
 
         /// <summary>
         /// 下钩
