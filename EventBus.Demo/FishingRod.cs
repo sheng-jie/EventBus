5d781ec0aedb5a591f83d8c41612b5673bab2273
shengjie_yan <ysjshengjie@live.cn>
2017-06-11 21:25:06 +0800

    移除无用代码

--- EventBus.Demo/FishingRod.cs
+++ EventBus.Demo/FishingRod.cs
@@ -9,30 +9,30 @@ namespace EventBus.Demo
     /// </summary>
     public class FishingRod
     {
-        public delegate void FishingHandler(FishingEventData eventData); //声明委托
-        public event FishingHandler FishingEvent; //声明事件
+        //public delegate void FishingHandler(FishingEventData eventData); //声明委托
+        //public event FishingHandler FishingEvent; //声明事件
 
         public FishingRod()
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
+            //Assembly assembly = Assembly.GetExecutingAssembly();
 
-            foreach (var type in assembly.GetTypes())
-            {
-                if (typeof(IEventHandler).IsAssignableFrom(type))//判断当前类型是否实现了IEventHandler接口
-                {
-                    Type handlerInterface = type.GetInterface("IEventHandler`1");//获取该类实现的泛型接口
-                    Type eventDataType = handlerInterface.GetGenericArguments()[0]; // 获取泛型接口指定的参数类型
+            //foreach (var type in assembly.GetTypes())
+            //{
+            //    if (typeof(IEventHandler).IsAssignableFrom(type))//判断当前类型是否实现了IEventHandler接口
+            //    {
+            //        Type handlerInterface = type.GetInterface("IEventHandler`1");//获取该类实现的泛型接口
+            //        Type eventDataType = handlerInterface.GetGenericArguments()[0]; // 获取泛型接口指定的参数类型
 
-                    //如果参数类型是FishingEventData，则说明事件源匹配
-                    if (eventDataType == typeof(FishingEventData))
-                    {
-                        //创建实例
-                        var handler = Activator.CreateInstance(type) as IEventHandler<FishingEventData>;
-                        //注册事件
-                        if (handler != null) FishingEvent += handler.HandleEvent;
-                    }
-                }
-            }
+            //        //如果参数类型是FishingEventData，则说明事件源匹配
+            //        if (eventDataType == typeof(FishingEventData))
+            //        {
+            //            //创建实例
+            //            var handler = Activator.CreateInstance(type) as IEventHandler<FishingEventData>;
+            //            //注册事件
+            //            if (handler != null) FishingEvent += handler.HandleEvent;
+            //        }
+            //    }
+            //}
         }
 
         /// <summary>
@@ -48,13 +48,12 @@ namespace EventBus.Demo
                 var a = new Random(10).Next();
                 var type = (FishType)new Random().Next(0, 5);
                 Console.WriteLine("铃铛：叮叮叮，鱼儿咬钩了");
-                if (FishingEvent != null)
-                {
-                    var eventData = new FishingEventData() { FishType = type, FishingMan = man };
-                    //FishingEvent(eventData);//不再需要通过事件委托触发
-                    EventBus.Default.Trigger(eventData);
-                    EventBus.Default.Trigger<FishingEventData>(eventData);//直接通过事件总线触发即可
-                }
+                //if (FishingEvent != null)
+                //{
+                var eventData = new FishingEventData() { FishType = type, FishingMan = man };
+                //FishingEvent(eventData);//不再需要通过事件委托触发
+                EventBus.Default.Trigger<FishingEventData>(eventData);//直接通过事件总线触发即可
+                //}
             }
         }
     }
--- EventBus.Demo/IEventHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EventBus.Demo
-{
-    public interface IEventHandler<T1, T2>
-    {
-    }
-}
