04577d0bbed13a748ef2d9e7894981d7f1b120fd
shengjie_yan <ysjshengjie@live.cn>
2017-06-12 19:46:11 +0800

    完善测试用例

--- EventBus.Test/EventBut_Simple_Test.cs
+++ EventBus.Test/EventBut_Simple_Test.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Xunit;
+﻿using Xunit;
 using Shouldly;
+using System;
+using System.Collections.Generic;
 
 namespace EventBus.Test
 {
-    [TestClass]
-    public class EventBut_Simple_Test: EventBusTestBase
+    public class EventBut_Simple_Test : EventBusTestBase
     {
         [Fact]
         public void Should_Call_Handler_On_Event_Has_Registered()
@@ -16,5 +15,25 @@ namespace EventBus.Test
             TestEventBus.Trigger<TestEventData>(new TestEventData(1));
             TestEventHandler.TestValue.ShouldBe(1);
         }
+
+        [Fact]
+        public void Should_Throw_Exception_Without_Registered()
+        {
+            Assert.Throws<KeyNotFoundException>(() => TestEventBus.Trigger<TestEventData>(new TestEventData(1)));
+        }
+
+        [Fact]
+        public void Should_Not_Trigger_On_UnRegistered()
+        {
+            Should_Call_Handler_On_Event_Has_Registered();
+
+            TestEventBus.UnRegister<TestEventData>(typeof(TestEventHandler));
+
+            TestEventBus.Trigger<TestEventData>(new TestEventData(2));
+
+            TestEventHandler.TestValue.ShouldBe(1);
+
+        }
+
     }
 }
