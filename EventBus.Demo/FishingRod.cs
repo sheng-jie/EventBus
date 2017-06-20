788d19b0dd8ac9debb5d6a2d84af155a0814a2ed
圣杰 <ysjshengjie@live.cn>
2017-06-20 08:34:22 +0800

    注释多余调用

--- EventBus.Demo/FishingRod.cs
+++ EventBus.Demo/FishingRod.cs
@@ -54,8 +54,8 @@ namespace EventBus.Demo
                 {
                     var eventData = new FishingEventData() { FishType = type, FishingMan = man };
                     //FishingEvent(eventData);//不再需要通过事件委托触发
-                    EventBus.Default.Trigger(eventData);
-                    EventBus.Default.Trigger<FishingEventData>(eventData);//直接通过事件总线触发即可
+                    EventBus.Default.Trigger(eventData);//直接通过事件总线触发即可
+                    //EventBus.Default.Trigger<FishingEventData>(eventData);
                 }
             }
         }
